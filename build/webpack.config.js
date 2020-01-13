c82f4732b8688925c05693194cb8fbfe140ba360
Cussone <aleksandar.cakalic@gmail.com>
2020-01-13 14:18:03 +0100

    Initial commit

---
+++ build/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/main.js',
+  output: {
+    filename: '[name].[hash].js',
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: './src',
+    compress: true,
+    port: 9090,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[hash].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/test.html',
+      filename: 'test.html',
+    }),
+  ],
+};
